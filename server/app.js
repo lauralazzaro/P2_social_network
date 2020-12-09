109673de67633977fb14ce93d528f6d360c272e0
Laura <lz_laura@hotmail.com>
2020-12-09 13:32:29 +0100

    Create DB if not exist

--- server/app.js
+++ server/app.js
@@ -7,8 +7,24 @@ const relations = require('./models/relations');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 
 require('dotenv').config();
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE projet7", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
 
 connection.authenticate()
     .then(() => console.log('Connected to DB'))
