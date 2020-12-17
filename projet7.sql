ae2dca23957d15bc03b9214e1e051b6a7d9dd098
Laura <lz_laura@hotmail.com>
2020-12-17 18:15:40 +0100

    new DB

--- projet7.sql
+++ projet7.sql
@@ -36,9 +36,9 @@ CREATE TABLE `comments` (
   UNIQUE KEY `id_comment` (`id_comment`),
   KEY `id_post` (`id_post`),
   KEY `id_user` (`id_user`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id_post`) ON UPDATE CASCADE,
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id_post`) ON DELETE SET NULL ON UPDATE SET NULL,
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,1,5,'This is a comment for the first post ever created','http://localhost:3000/images/moutain1607609058437.jpg','2020-12-09 14:35:21','2020-12-10 14:04:18'),(5,3,5,'Created with a text area',NULL,'2020-12-11 10:29:04','2020-12-11 10:29:04');
+INSERT INTO `comments` VALUES (1,1,5,'This is a comment for the first post ever created','http://localhost:3000/images/moutain1607609058437.jpg','2020-12-09 14:35:21','2020-12-10 14:04:18'),(5,3,5,'Created with a text area',NULL,'2020-12-11 10:29:04','2020-12-11 10:29:04'),(6,5,8,'Welcome!!','http://localhost:3000/images/welcome1607877244325.jpg','2020-12-13 16:34:04','2020-12-13 16:34:04'),(18,6,8,'Personne???? - modify ',NULL,'2020-12-13 23:21:06','2020-12-16 13:59:05'),(20,6,6,'Who wrote this post?',NULL,'2020-12-14 09:40:35','2020-12-14 09:40:35'),(21,4,8,'Me too!!!!',NULL,'2020-12-14 13:30:16','2020-12-14 13:30:16');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,16 +63,13 @@ CREATE TABLE `posts` (
   `id_user` int DEFAULT NULL,
   `text` text,
   `imageUrl` varchar(255) DEFAULT NULL,
-  `id_subject` int DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id_post`),
   UNIQUE KEY `id_post` (`id_post`),
   KEY `id_user` (`id_user`),
-  KEY `id_subject` (`id_subject`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE,
-  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`id_subject`) REFERENCES `subjects` (`id_subject`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -81,7 +78,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,5,'This is the first post created and it has an image. (modified)','http://localhost:3000/images/stars1607593527539.jpg',NULL,'2020-12-09 14:34:40','2020-12-11 10:26:07'),(2,5,'Just created a second post but the image was added after and the text was modified',NULL,NULL,'2020-12-09 14:36:57','2020-12-09 15:07:48'),(3,6,'this is a very nice office! ','http://localhost:3000/images/office1607592758001.jpg',NULL,'2020-12-10 09:32:38','2020-12-10 09:32:38'),(4,6,'I would like be on holiday!!!','http://localhost:3000/images/party1607608981998.jpg',NULL,'2020-12-10 09:39:24','2020-12-10 14:03:02'),(5,7,'Hey!! it\'s nice to be here!!',NULL,NULL,'2020-12-10 09:55:45','2020-12-10 09:55:45');
+INSERT INTO `posts` VALUES (1,5,'This is the first post created and it has an image. (modified)','http://localhost:3000/images/stars1607593527539.jpg','2020-12-09 14:34:40','2020-12-11 10:26:07'),(2,5,'Just created a second post but the image was added after and the text was modified',NULL,'2020-12-09 14:36:57','2020-12-09 15:07:48'),(3,6,'this is a very nice office! ','http://localhost:3000/images/office1607592758001.jpg','2020-12-10 09:32:38','2020-12-10 09:32:38'),(4,6,'I would like be on holiday!!!','http://localhost:3000/images/party1607608981998.jpg','2020-12-10 09:39:24','2020-12-10 14:03:02'),(5,7,'Hey!! it\'s nice to be here!!',NULL,'2020-12-10 09:55:45','2020-12-10 09:55:45'),(6,8,'Vous avez vu le dernier episode???  ','http://localhost:3000/images/star trek discovery1607878256094.jpg','2020-12-13 16:50:56','2020-12-13 16:50:56'),(9,NULL,'Test post for delete account',NULL,'2020-12-16 14:03:05','2020-12-16 14:03:05');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,32 +110,6 @@ INSERT INTO `roles` VALUES (1,'user','2020-12-09 00:00:00','2020-12-09 00:00:00'
 UNLOCK TABLES;
 
 --
--- Table structure for table `subjects`
---
-
-DROP TABLE IF EXISTS `subjects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subjects` (
-  `id_subject` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id_subject`),
-  UNIQUE KEY `id_subject` (`id_subject`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subjects`
---
-
-LOCK TABLES `subjects` WRITE;
-/*!40000 ALTER TABLE `subjects` DISABLE KEYS */;
-/*!40000 ALTER TABLE `subjects` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -158,8 +129,8 @@ CREATE TABLE `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`),
   KEY `id_role` (`id_role`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -168,7 +139,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'laura@email.com','$2b$10$fk3YKqMh3eV24aAHzdWrQ.UehJcf65WiHJ1.xUSVfwbUKktsGAjtS','Laura',1,'2020-12-09 14:31:32','2020-12-09 14:31:32'),(6,'moderator@email.com','$2b$10$IdTTn00ax2r5XAhbPGoJ0uDt8DUkmvTksZNcpxfJytQ.kCRMghUqK','Moderator',2,'2020-12-09 16:04:30','2020-12-09 16:04:30'),(7,'jean@email.com','$2b$10$6UUZi8FucvKTFp6kJOpCLOl9HfDkkfzkzx7onA46YFaM1.WflNoPi','Jean',1,'2020-12-10 09:55:21','2020-12-10 09:55:21');
+INSERT INTO `users` VALUES (5,'laura2@email.com','$2b$10$fk3YKqMh3eV24aAHzdWrQ.UehJcf65WiHJ1.xUSVfwbUKktsGAjtS','Laura A.',1,'2020-12-09 14:31:32','2020-12-09 14:31:32'),(6,'moderator@email.com','$2b$10$IdTTn00ax2r5XAhbPGoJ0uDt8DUkmvTksZNcpxfJytQ.kCRMghUqK','Moderator',2,'2020-12-09 16:04:30','2020-12-09 16:04:30'),(7,'jean@email.com','$2b$10$6UUZi8FucvKTFp6kJOpCLOl9HfDkkfzkzx7onA46YFaM1.WflNoPi','Jean',1,'2020-12-10 09:55:21','2020-12-10 09:55:21'),(12,'laura@email.com','$2b$10$MCEbqGMGyVCVhCE6bvbrMOmNOGnSIwQiWubsDjegMXBNsZonVImS6','laura l.',1,'2020-12-14 15:08:47','2020-12-14 15:08:47');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -181,4 +152,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-11 15:40:02
+-- Dump completed on 2020-12-17 17:46:28
